c31366393a13e74fb0c6fb9cb8e8266a01fb3b32
similo919 <zwelizwane54@gmail.com>
2026-06-23 15:28:56 +0200

    feat(menu): MenuRepository

--- src/main/java/food_ordering_system/Repository/MenuRepository.java
+++ src/main/java/food_ordering_system/Repository/MenuRepository.java
@@ -1,4 +1,7 @@
 package food_ordering_system.Repository;
 
-public class MenuRepository {
+import food_ordering_system.entity.Menu;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MenuRepository extends JpaRepository<Menu, Long> {
 }
