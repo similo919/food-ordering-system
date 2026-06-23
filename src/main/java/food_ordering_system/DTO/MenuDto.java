7669457aed9f5f9ace521dfe784d9f7f5d5e8497
similo919 <zwelizwane54@gmail.com>
2026-06-23 15:30:38 +0200

    feat(menu): MenuDto with validation

--- src/main/java/food_ordering_system/DTO/MenuDto.java
+++ src/main/java/food_ordering_system/DTO/MenuDto.java
@@ -1,4 +1,86 @@
 package food_ordering_system.DTO;
 
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.math.BigDecimal;
+
 public class MenuDto {
+
+    private Long id;
+
+    @NotBlank(message = "Menu name is required")
+    private String name;
+
+    private String description;
+
+    @NotNull(message = "Price is required")
+    @DecimalMin(value = "0.0", message = "Price must be greater than 0")
+    private BigDecimal price;
+
+    private String imageUrl;
+
+    @NotNull(message = "Category Id is required")
+    private Long categoryId;
+
+    private String categoryName;
+
+    public MenuDto() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public Long getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Long categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
 }
